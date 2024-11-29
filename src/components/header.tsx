109631c2227ae3eb640cf3dd60fe82e2122dd772
Alan Olimpio <alancleyton.olimpio@gmail.com>
2024-11-29 17:58:52 -0300

    feat: cria menu

---
+++ src/components/header.tsx
@@ -0,0 +1,51 @@
+import { Home, Pizza, UtensilsCrossed } from "lucide-react";
+import { useState } from "react";
+
+import { NavLink } from "./nav-link";
+import { Separator } from "./ui/separator";
+
+export function Header() {
+  const [toggle, setToggle] = useState(false);
+
+  function handleClickToggle() {
+    setToggle(!toggle);
+  }
+  return (
+    <div className="border-b">
+      <div className="flex min-h-16 w-full flex-wrap items-center justify-between gap-6 px-6 pb-8 pt-8 md:justify-start">
+        <Pizza className="h-6 w-6" />
+        <Separator orientation="vertical" className="h-6 pt-8" />
+        <a href="#" onClick={handleClickToggle}>
+          <svg
+            xmlns="<http://www.w3.org/2000/svg>"
+            id="menu-button"
+            className="block h-6 w-6 cursor-pointer md:hidden"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </a>
+        <nav
+          className={`w-full gap-4 md:visible md:flex md:w-auto md:items-center ${toggle ? "visibile" : "hidden"}`}
+          id="menu"
+        >
+          <NavLink to="/">
+            <Home className="h-4 w-4" />
+            Início
+          </NavLink>
+          <NavLink to="/orders">
+            <UtensilsCrossed className="h-4 w-4" />
+            Pedidos
+          </NavLink>
+        </nav>
+      </div>
+    </div>
+  );
+}
--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -2,7 +2,7 @@ import { Pizza } from "lucide-react";
 import { Outlet } from "react-router-dom";
 export function AuthLayout() {
   return (
-    <div className="grid md:min-h-screen md:grid-cols-2">
+    <div className="grid antialiased md:min-h-screen md:grid-cols-2">
       <div className="flex flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground max-md:h-full">
         <div className="flex items-center gap-3 text-lg font-medium text-foreground">
           <Pizza className="h-5 w-5" />

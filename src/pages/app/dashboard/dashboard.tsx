274f90afb8d03a09521f7d033ee8b0ccb686f158
Alan Olimpio <alancleyton.olimpio@gmail.com>
2024-12-06 10:33:09 -0300

    feat: cria componente MonthRevenueCard

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -1,10 +1,16 @@
 import { Helmet } from "react-helmet-async";
 
+import { MonthRevenueCard } from "./month-revenue-card";
 export function Dashboard() {
   return (
     <>
       <Helmet title="Dashboard" />
-      <h1>Dashboard</h1>
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+        <div className="grid grid-cols-4 gap-4 max-md:grid-cols-2 max-sm:grid-cols-1">
+          <MonthRevenueCard />
+        </div>
+      </div>
     </>
   );
 }

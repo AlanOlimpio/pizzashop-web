01d1a8f3b94bc5d71f056ff2283dc0384af31547
Alan Olimpio <alancleyton.olimpio@gmail.com>
2024-12-16 14:44:40 -0300

    feat: cria OrderTableSkeleton

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -14,6 +14,7 @@ import {
 } from "@/components/ui/table";
 
 import { OrderTableFilters } from "./order-table-filters";
+import { OrderTableSkeleton } from "./order-table-skeleton";
 import { OrderTableRow } from "./orders-table-row";
 
 export function Orders() {
@@ -28,7 +29,7 @@ export function Orders() {
     .transform((page) => page - 1)
     .parse(searchParams.get("page") ?? "1");
 
-  const { data: result } = useQuery({
+  const { data: result, isLoading: isLoadingOrders } = useQuery({
     queryKey: ["orders", pageIndex, orderId, customerName, status],
     queryFn: () =>
       getOrders({
@@ -78,6 +79,7 @@ export function Orders() {
             </TableBody>
           </Table>
         </div>
+        {isLoadingOrders && <OrderTableSkeleton />}
         {result && (
           <Pagination
             onPageChange={handlePaginate}
